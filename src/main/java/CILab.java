6b543bcc060ee016bd88011146a2d18b4620f896
Jonathan Jarman <jonathan.k.jarman@gmail.com>
2023-06-09 13:43:22 -0700

    Adding Comments

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -16,19 +16,19 @@ public class CILab implements CILabInterface {
         if (myString == null || myString.isEmpty())
             return true;
 
-        boolean allUpperCase = true;
-        boolean allLowerCase = true;
-        boolean onlyFirstCapital = true;
+        boolean allUpperCase = true; // Flag to track if all letters are uppercase
+        boolean allLowerCase = true;  // Flag to track if all letters are lowercase
+        boolean onlyFirstCapital = true; // Flag to track if only the first letter is capital
 
         for (int i = 0; i < myString.length(); i++) {
             char c = myString.charAt(i);
             if (Character.isLowerCase(c)) {
-                allUpperCase = false;
+                allUpperCase = false; // Lowercase letter found, not all letters are uppercase
                 if (i == 0) {
                     onlyFirstCapital = false;
                 }
             } else if (Character.isUpperCase(c)) {
-                allLowerCase = false;
+                allLowerCase = false; // Uppercase letter found, not all letters are lowercase
                 if (i > 0) {
                     onlyFirstCapital = false;
                 }
@@ -36,7 +36,7 @@ public class CILab implements CILabInterface {
                 allLowerCase = false;
                 allUpperCase = false;
                 if (i > 0) {
-                    onlyFirstCapital = false;
+                    onlyFirstCapital = false; // Uppercase letter found after the first letter, not only the first letter is capital
                 }
             }
         }
