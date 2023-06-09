f86eb31ccd8ff87097d800a46a94c24a701bd5b2
Jonathan Jarman <jonathan.k.jarman@gmail.com>
2023-06-09 11:59:23 -0700

    Added Method Implementations

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,8 +13,34 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
-    }
+        if (myString == null || myString.isEmpty())
+            return true;
 
+        boolean allUpperCase = true;
+        boolean allLowerCase = true;
+        boolean onlyFirstCapital = true;
+
+        for (int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if (Character.isLowerCase(c)) {
+                allUpperCase = false;
+                if (i == 0) {
+                    onlyFirstCapital = false;
+                }
+            } else if (Character.isUpperCase(c)) {
+                allLowerCase = false;
+                if (i > 0) {
+                    onlyFirstCapital = false;
+                }
+            } else {
+                allLowerCase = false;
+                allUpperCase = false;
+                if (i > 0) {
+                    onlyFirstCapital = false;
+                }
+            }
+        }
+        return allUpperCase || allLowerCase || onlyFirstCapital;
+    }
 }
 
